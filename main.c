be8995feb4e6cf0e520dd213fb7ced761dd6c4df
eilon <toledanoeilon3@gmail.com>
2021-12-08 19:37:29 +0200

    bbb

--- main.c
+++ main.c
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define ROWS 4
-#define COLS 6
+//#define ROWS 4
+//#define COLS 6
 #define ITERS 10
 
-#define dx 1
-#define dy 1
+#define dx 5
+#define dy 5
 
 #define numprocs 4
+
+
+#define ROWS 12
+#define COLS 22
+
+// overlap size 
+#define OVS (COLS-2)/4 + 1 
 
 
 // global vars 
 
 int my_id;
 MPI_Status status;
+int ROWS, COLS;
+
 
 
 void print_A(float A[][COLS]){
@@ -92,34 +101,167 @@ void iterate(float A[][COLS], int * overlaps_recv_address[numprocs], int * overl
 int main(int argc, char** argv) {
 
     // for debug only
-    int i = 1;
-    while(i);
+    int p_id = 1;
+    while(p_id);
+    
+
+    int numprocs2;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs2);
+    if(numprocs2 != numprocs){
+        printf("num of process is not 4!");
+        return 1;
+    }
 
 
-    // receive from master
-    float A[ROWS][COLS] = {{60 ,60 ,60 ,60 ,60 ,60 },
+
+    float map[ROWS][COLS] = { 0 };
+
+	if (myid == 0)
+	{											//Sending procs. 1 it's submap to calculate
+		float submap1[ROWS1][COLS1] = { 0 };
+
+		for (int j = 0; j < COLS1; j++) {				// initializing horizontal boundary condition
+			submap1[0][j] = 60;
+			submap1[ROWS1-1][j] = (j < 5 || j >= 15) ? 40 : 20;
+			//print("%d, %d", submap1[0][j], submap1[101][j]);
+		}
+		for (int i = 0; i < ROWS1; i++) {				// initializing vertical boundary condition
+			submap1[i][0] = 60;
+			submap1[i][COLS1-1] = 60;
+		}
+		
+		for (int i = 1; i < ROWS1 - 1; i++) { // initializing inner values
+			for (int j = 1; j < COLS1-1; j++)
+				submap1[i][j] = 40;
+		}
+
+		for (int i = 0; i < ROWS1; i++) {
+			for (int j = 0; j < COLS1; j++) {
+				printf("%.2f\t", submap1[i][j]);
+			}
+			printf("\n");
+			
+		}
+
+        float A[ROWS][COLS] = {{60 ,60 ,60 ,60 ,60 ,60 },
                            {60 ,40 ,40 ,40 ,40 ,60 },
                            {60 ,40 ,40 ,40 ,40 ,60 },
                            {40 ,40 ,20 ,20 ,40 ,40 }};
+        
+        int overlaps_recv_sizes[numprocs][numprocs] = { {0,0,OVS,OVS},      // process 0 has OVS with process 2 & 3
+                                                        {0,0,OVS,OVS},
+                                                        {OVS,OVS,0,0},
+                                                        {OVS,OVS,0,0}
+                                                        };  
+
+        int overlaps_recv_indexes[numprocs][numprocs][2] = { { {-1,-1}, {-1,-1}, {0,0}, {0,COLS-1- OVS}},   // no overlaps with process 2 start at [0][0] and with process 3 at [0][COLS-1- OVS]
+                                                             { {-1,-1}, {-1,-1}, {ROWS-1,0}, {ROWS-1,COLS-1- OVS}},
+                                                             { {ROWS-1,COLS-1- OVS}, {0,COLS-1- OVS}, {-1,-1},{-1,-1}},
+                                                             { {ROWS-1,0}, {0,0}, {-1,-1}, {-1,-1}}
+                                                            };
+
+        int overlaps_send_sizes[numprocs][numprocs] = { {0,0,OVS,OVS},      // process 0 has OVS with process 2 & 3
+                                                        {0,0,OVS,OVS},
+                                                        {OVS,OVS,2,0},
+                                                        {OVS,OVS,2,0}
+                                                        };  
+                                                        
+        int overlaps_send_indexes[numprocs][numprocs][2] = { { {-1,-1}, {-1,-1}, {1,1}, {1, COLS - 1 - OVS}},   // no overlaps with process 2 start at [0][0] and with process 3 at [0][COLS-1- OVS]
+                                                             { {-1,-1}, {-1,-1}, {ROWS-1-1,0}, {1, COLS - 1 - OVS}},
+                                                             { {ROWS-1-1,COLS-1- OVS}, {1,COLS-1- OVS}, {-1,-1},{-1,-1}},
+                                                             { {ROWS-1,0}, {1,0}, {-1,-1}, {-1,-1}}
+                                                            };
+		for(p_id = 1; p_id < numprocs; p_id++){
+
+            MPI_Send(submap1,
+                ROWS1*COLS1,
+                MPI_FLOAT,
+                p_id,				// destination process
+                1,
+                MPI_COMM_WORLD);
+
+            MPI_Send(overlaps_recv_sizes[p_id],
+                numprocs,
+                MPI_INT,
+                p_id,				// destination process
+                2,
+                MPI_COMM_WORLD);
+
+            MPI_Send(overlaps_recv_indexes[p_id],
+                numprocs*2,
+                MPI_INT,
+                p_id,				// destination process
+                3,
+                MPI_COMM_WORLD);
+
+            MPI_Send(overlaps_send_sizes[p_id],
+                numprocs,
+                MPI_INT,
+                p_id,				// destination process
+                4,
+                MPI_COMM_WORLD);
+
+            MPI_Send(overlaps_send_indexes[p_id],
+                numprocs*2,
+                MPI_INT,
+                p_id,				// destination process
+                5,
+                MPI_COMM_WORLD);
+        }
 
 
-    int overlaps_recv_sizes[numprocs] = {0,2,2,0};     // represent the num of cells that the process has in common
-
-    int overlaps_recv_indexes[numprocs][2] = {{-1,-1},  // no overlaps with itself
-                                                  {3,0},    // over laps with process 2 start at index [3][0] until [3][1]
-                                                  {3,4},    // over laps with process 3 start at index [3][4] until [3][5]
-                                                  {-1,-1}   // no overlaps with process 4
-                                                  };
-
-    int overlaps_send_sizes[numprocs] = {0,1,1,0};     // represent the num of cells that the process has in common
-
-    int overlaps_send_indexes[numprocs][2] = {{-1,-1},  // no overlaps with itself
-                                                  {2,1},    // over laps with process 2 start at index [3][0] until [3][1]
-                                                  {2,4},    // over laps with process 3 start at index [3][4] until [3][5]
-                                                  {-1,-1}   // no overlaps with process 4
-                                                  };
 
 
+    /// all the process do the following
+    else{   // my_id !=  0
+        
+        float A[ROWS][COLS];
+
+		MPI_Recv(A,
+			ROWS*COLS,
+			MPI_FLOAT,
+			0,				// destination process
+			1,
+			MPI_COMM_WORLD,
+            status);
+
+
+		MPI_Recv(overlaps_recv_sizes,
+			numprocs,
+			MPI_INT,
+			0,				// destination process
+			2,
+			MPI_COMM_WORLD,
+            status);
+
+
+		MPI_Recv(overlaps_recv_indexes,
+			numprocs*2,
+			MPI_INT,
+			0,				// destination process
+			3,
+			MPI_COMM_WORLD,
+            status);
+
+
+		MPI_Recv(overlaps_send_sizes,
+			numprocs,
+			MPI_INT,
+			0,				// destination process
+			4,
+			MPI_COMM_WORLD,
+            status);
+
+
+		MPI_Recv(overlaps_send_indexes,
+			numprocs*2,
+			MPI_INT,
+			0,				// destination process
+			5,
+			MPI_COMM_WORLD,
+            status);
+    }
 
     int * overlaps_recv_address[numprocs];
     get_overlaps_address(A, overlaps_recv_address, overlaps_recv_indexes);
